2adb321995b544fbca48014d32cd94ee7645f4cf
DenisKolgatin <denissova1453@gmail.com>
2025-12-15 23:51:08 +0300

    correct

--- src/test/RandomsTest.java
+++ src/test/RandomsTest.java
@@ -3,8 +3,6 @@ package test;
 
 import main.Randoms;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 
 import java.util.*;
